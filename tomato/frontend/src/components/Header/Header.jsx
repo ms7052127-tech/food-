f1bc9463667cb868eb0143fd3b476231d67ecc9b
ms7052127-tech <ms7052127@gmail.com>
2025-09-30 17:16:36 +0530

    first commit

---
+++ tomato/frontend/src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order Your favourite food here</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum doloremque
+             doloribus rerum, veritatis perferendis unde architecto itaque debitis, 
+             possimus, temporibus consequatur provident aut porro enim suscipit quas 
+             minus laborum fugiat!</p>
+             <button>view menu</button>
+      </div>
+
+    </div>
+  )
+}
+
+export default Header
